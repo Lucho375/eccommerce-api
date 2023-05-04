73456a15d653835909708f4eaec6ef916f8d9fa5
Luciano <luciano.rodriguez59@gmail.com>
2023-05-03 21:48:33 -0300

    modified scripts and config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,13 @@
 import 'dotenv/config'
+
+const DB =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_DB_URI
+    : process.env.MONGO_DB_LOCALHOST
+
 export default {
   SERVER_PORT: process.env.PORT,
-  MONGO_DB_URI: process.env.MONGO_DB_URI,
+  MONGO_DB_URI: DB,
   corsOptions: {
     origin: '*'
   }
