1a891ae1452a2a6ff3b8b2fb64ce7965222d37c6
Luciano <luciano.rodriguez59@gmail.com>
2023-06-24 00:48:37 -0300

    index, vercel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import DbFactory from './data/factories/dbFactory.js'
 import AppFactory from './presentation/factories/appFactory.js'
 import config from './config/index.js'
-
-const db = DbFactory.create()
-db.init(config.MONGO_DB_URI)
-const app = AppFactory.create()
-app.init()
-app.build()
-app.listen()
+;(async function () {
+  const db = DbFactory.create()
+  db.init(config.MONGO_DB_URI)
+  const app = AppFactory.create()
+  app.init()
+  app.build()
+  app.listen()
+})()
