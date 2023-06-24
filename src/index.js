20039afa805980523f3576eb2ce251f3ca3bdb0d
Luciano <luciano.rodriguez59@gmail.com>
2023-06-24 00:46:14 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import DbFactory from './data/factories/dbFactory.js'
 import AppFactory from './presentation/factories/appFactory.js'
 import config from './config/index.js'
-;(async function () {
-  const db = DbFactory.create()
-  db.init(config.MONGO_DB_URI)
-  const app = AppFactory.create()
-  app.init()
-  app.build()
-  app.listen()
-})()
+
+const db = DbFactory.create()
+db.init(config.MONGO_DB_URI)
+const app = AppFactory.create()
+app.init()
+app.build()
+app.listen()
