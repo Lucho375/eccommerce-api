6f6ec96d8a9925890261076e0ca87a87f182843a
Luciano <luciano.rodriguez59@gmail.com>
2023-06-24 00:53:05 -0300

    package.json

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ import 'dotenv/config'
 
 export default {
   SERVER_PORT: process.env.PORT,
-  MONGO_DB_URI: process.env.NODE_ENV === 'production' ? process.env.MONGO_DB_URI : process.env.MONGO_DB_LOCALHOST,
+  // MONGO_DB_URI: process.env.NODE_ENV === 'production' ? process.env.MONGO_DB_URI : process.env.MONGO_DB_LOCALHOST,
+  MONGO_DB_URI: process.env.MONGO_DB_URI,
   corsOptions: {
     origin: 'http://localhost:5173',
     credentials: true
