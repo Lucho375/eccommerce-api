be1fb346476611f0199476792ccc176e5de66565
Luciano <luciano.rodriguez59@gmail.com>
2023-07-31 19:52:49 -0300

    modified swagger

--- src/swagger.js
+++ src/swagger.js
@@ -1,9 +1,7 @@
 import swaggerJSDoc from 'swagger-jsdoc'
+import { SwaggerTheme } from 'swagger-themes'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
-
-import { SwaggerTheme } from 'swagger-themes'
-const theme = new SwaggerTheme('v3') // Specifying the Swagger Version
 
 const currentFileUrl = import.meta.url
 const currentFilePath = fileURLToPath(currentFileUrl)
@@ -17,10 +15,11 @@ const options = {
       version: '1.0.0'
     }
   },
-  apis: [resolve(currentDir, './docs/**/*.yaml')],
-  customCss: theme.getBuffer('dark')
+  apis: [resolve(currentDir, './docs/**/*.yaml')]
 }
 
 export const specs = swaggerJSDoc(options)
+
+const theme = new SwaggerTheme('v3')
 
 export const swaggerTheme = { customCss: theme.getBuffer('dark'), customSiteTitle: 'ECCOMMERCE API' }
